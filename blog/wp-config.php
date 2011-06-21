b71f73bcd66a28d9c4028a11dfc4c52089bc6711
unknown <bigzel@gmail.com>
2011-06-21 12:18:14 +0300

    v1

--- blog/wp-config.php
+++ blog/wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'v2340_mono');
+define('DB_NAME', 'db');
 
 /** MySQL database username */
-define('DB_USER', 'v2340_mono');
+define('DB_USER', 'u');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'mono');
+define('DB_PASSWORD', 'psw');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
