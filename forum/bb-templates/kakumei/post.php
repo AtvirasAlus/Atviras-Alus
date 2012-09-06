7e84cd99605b63008e67abcee1765fa11b6f8dae
Tomas Žeimys <tomas@tomaszeimys.lt>
2012-09-06 23:17:13 +0300

    idea#55

--- forum/bb-templates/kakumei/post.php
+++ forum/bb-templates/kakumei/post.php
@@ -10,7 +10,7 @@
 			<?php post_text(); ?>
 		</div>
 		<div class="recipe_comment_date">
-			<?php printf( __('Posted %s ago'), bb_get_post_time() ); ?> <a href="<?php post_anchor_link(); ?>">#</a> <?php bb_post_admin(); ?>
+			<span title="<?=date("Y-m-d H:i:s", get_post_timestamp())?>"><?php printf( __('Posted %s ago'), bb_get_post_time() ); ?></span> <a href="<?php post_anchor_link(); ?>">#</a> <?php bb_post_admin(); ?>
 		</div>
 	</div>
 	<div class="clear"></div>
