dd9b581ae3b60ffbc4e628a6ba81292b606c6d6c
Tomas Žeimys <tomas@tomaszeimys.lt>
2012-11-23 18:31:12 +0200

    wiki

---
+++ wiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+root:$1$9MMCF894$KEIRb/jNCkK3REpiAaOz51:root:tomas@tomaszeimys.lt:admin,user
