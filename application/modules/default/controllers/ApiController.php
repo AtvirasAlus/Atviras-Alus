146136639453ef4f82d2ce3dfc9d6891482d5c4f
Tomas Žeimys <tomas@tomaszeimys.lt>
2013-01-03 07:26:39 +0200

    api frame

--- application/modules/default/controllers/ApiController.php
+++ application/modules/default/controllers/ApiController.php
@@ -90,7 +90,7 @@ class ApiController extends Zend_Controller_Action {
 				->join("users", "users.user_id = beer_recipes.brewer_id", array("user_name"))
 				->order("recipe_name ASC");
 		$result = $db->FetchAll($select);
-		echo "<pre>";print_r($result);exit;
+		//echo "<pre>";print_r($result);exit;
 		$response['status'] = "1";
 		$response['count'] = sizeof($result);
 		$response['items'] = $result;
