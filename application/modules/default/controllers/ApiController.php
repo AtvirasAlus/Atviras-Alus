ad04d93cd5061d2f98436cb179a8e8304574144d
Tomas Žeimys <tomas@tomaszeimys.lt>
2013-01-12 18:38:25 +0200

    API v2

--- application/modules/default/controllers/ApiController.php
+++ application/modules/default/controllers/ApiController.php
@@ -187,7 +187,7 @@ class ApiController extends Zend_Controller_Action {
 		echo $_GET['callback'] . '(' . json_encode($response) . ")";
 	}
 
-	
+	// API 2.0
 	
 	public function getalluserrecipesAction(){
 		$this->_helper->layout->setLayout('empty');
@@ -247,4 +247,173 @@ class ApiController extends Zend_Controller_Action {
 		echo $_GET['callback'] . '(' . json_encode($response) . ")";
 	}
 	
+	public function getalluserfavoriterecipesAction(){
+		$this->_helper->layout->setLayout('empty');
+		$this->_helper->viewRenderer->setNoRender(true);
+		$response = array();
+		$response['status'] = "0";
+		$db = Zend_Registry::get("db");
+		$token = trim($this->_getParam('token'));
+		$uid = $this->getuserid($token);
+		if ($uid === false){
+			$response['status'] = 999;
+			echo json_encode($response);
+			exit;
+		}
+		$response['status'] = "1";
+		$select = $db->select()
+				->from("beer_recipes_favorites", array())
+				->join("beer_recipes", "beer_recipes.recipe_id=beer_recipes_favorites.recipe_id")
+				->where("beer_recipes_favorites.user_id = ?", $uid)
+				->join("beer_styles", "beer_styles.style_id = beer_recipes.recipe_style", array("style_name"))
+				->join("users", "users.user_id = beer_recipes.brewer_id", array("user_name"))
+				->order("beer_recipes.recipe_name ASC");
+		$result = $db->FetchAll($select);
+		$response['recipes_count'] = sizeof($result);
+		$response['recipes'] = $result;
+		$rids = array();
+		foreach($result as $key=>$val){
+			$rids[] = $val['recipe_id'];
+		}
+		$rsize = sizeof($rids);
+		$rids = implode(", ", $rids);
+		if ($rsize > 0){
+			$select = $db->select()
+					->from("beer_awards")
+					->where("recipe_id IN (".$rids.")");
+			$result = $db->FetchAll($select);
+			$response['awards_count'] = sizeof($result);
+			$response['awards'] = $result;
+			$select = $db->select()
+					->from("beer_recipes_hops")
+					->where("recipe_id IN (".$rids.")");
+			$result = $db->FetchAll($select);
+			$response['hops_count'] = sizeof($result);
+			$response['hops'] = $result;
+			$select = $db->select()
+					->from("beer_recipes_malt")
+					->where("recipe_id IN (".$rids.")");
+			$result = $db->FetchAll($select);
+			$response['malts_count'] = sizeof($result);
+			$response['malts'] = $result;
+			$select = $db->select()
+					->from("beer_recipes_yeast")
+					->where("recipe_id IN (".$rids.")");
+			$result = $db->FetchAll($select);
+			$response['yeasts_count'] = sizeof($result);
+			$response['yeasts'] = $result;
+		}
+		echo $_GET['callback'] . '(' . json_encode($response) . ")";
+	}
+	public function getalluserrecipecommentsAction(){
+		$this->_helper->layout->setLayout('empty');
+		$this->_helper->viewRenderer->setNoRender(true);
+		$response = array();
+		$response['status'] = "0";
+		$db = Zend_Registry::get("db");
+		$token = trim($this->_getParam('token'));
+		$uid = $this->getuserid($token);
+		if ($uid === false){
+			$response['status'] = 999;
+			echo json_encode($response);
+			exit;
+		}
+		$response['status'] = "1";
+		$select = $db->select()
+				->from("beer_recipes_favorites", array("recipe_id"))
+				->where("beer_recipes_favorites.user_id = ?", $uid);
+		$result = $db->FetchAll($select);
+		$rids = array();
+		foreach($result as $key=>$val){
+			$rids[] = $val['recipe_id'];
+		}
+		$select = $db->select()
+				->from("beer_recipes", array("recipe_id"))
+				->where("brewer_id = ?", $uid);
+		$result = $db->FetchAll($select);
+		foreach($result as $key=>$val){
+			$rids[] = $val['recipe_id'];
+		}
+		$rids = array_unique($rids);
+		$response['items_size'] = 0;
+		$response['items'] = array();
+		if (sizeof($rids) > 0){
+			$rids = implode(", ", $rids);
+			$select = $db->select()
+					->from("beer_recipes_comments")
+					->join("users", "beer_recipes_comments.comment_brewer=users.user_id", array("user_name", "user_email"))
+					->where("beer_recipes_comments.comment_recipe IN (".$rids.")");
+			$result = $db->FetchAll($select);
+			$response['items_size'] = sizeof($result);
+			$response['items'] = $result;
+		}
+		echo $_GET['callback'] . '(' . json_encode($response) . ")";
+	}
+	public function getallusersessionsAction(){
+		$this->_helper->layout->setLayout('empty');
+		$this->_helper->viewRenderer->setNoRender(true);
+		$response = array();
+		$response['status'] = "0";
+		$db = Zend_Registry::get("db");
+		$token = trim($this->_getParam('token'));
+		$uid = $this->getuserid($token);
+		if ($uid === false){
+			$response['status'] = 999;
+			echo json_encode($response);
+			exit;
+		}
+		$response['status'] = "1";
+		$select = $db->select()
+				->from("beer_brew_sessions")
+				->join("beer_recipes", "beer_brew_sessions.session_recipe=beer_recipes.recipe_id", array("recipe_name"))
+				->where("beer_brew_sessions.session_brewer = ?", $uid);
+		$result = $db->FetchAll($select);
+		$response['items_size'] = sizeof($result);
+		$response['items'] = $result;
+		echo $_GET['callback'] . '(' . json_encode($response) . ")";
+	}
+	public function getalluserstorageAction(){
+		$this->_helper->layout->setLayout('empty');
+		$this->_helper->viewRenderer->setNoRender(true);
+		$response = array();
+		$response['status'] = "0";
+		$db = Zend_Registry::get("db");
+		$token = trim($this->_getParam('token'));
+		$uid = $this->getuserid($token);
+		if ($uid === false){
+			$response['status'] = 999;
+			echo json_encode($response);
+			exit;
+		}
+		$response['status'] = "1";
+		$select = $db->select()
+				->from("storage_hops")
+				->where("user_id = ?", $uid)
+				->order("hop_name");
+		$result = $db->FetchAll($select);
+		$response['hops_size'] = sizeof($result);
+		$response['hops'] = $result;
+		$select = $db->select()
+				->from("storage_malt")
+				->where("user_id = ?", $uid)
+				->order("malt_name");
+		$result = $db->FetchAll($select);
+		$response['malt_size'] = sizeof($result);
+		$response['malt'] = $result;
+		$select = $db->select()
+				->from("storage_other")
+				->where("user_id = ?", $uid)
+				->order("other_name");
+		$result = $db->FetchAll($select);
+		$response['other_size'] = sizeof($result);
+		$response['other'] = $result;
+		$select = $db->select()
+				->from("storage_yeast")
+				->where("user_id = ?", $uid)
+				->order("yeast_name");
+		$result = $db->FetchAll($select);
+		$response['yeast_size'] = sizeof($result);
+		$response['yeast'] = $result;
+		echo $_GET['callback'] . '(' . json_encode($response) . ")";
+	}
 }
